1a3824f650ce4c8a2453f69cf26f791a64e7ef94
Viacheslav Strogalev <strogalev.vyacheslav@gmail.com>
2021-03-14 20:51:48 +0300

    Testimonials with carousel are done. Carousel is not match to design

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,17 +1,70 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.promo__nav-menu'),
-    menuItem = document.querySelectorAll('.promo__nav-item'),
-    hamburger = document.querySelector('.hamburger');
+	const menu = document.querySelector('.promo__nav-menu'),
+		menuItem = document.querySelectorAll('.promo__nav-item'),
+		hamburger = document.querySelector('.hamburger');
 
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('promo__nav-menu_active');
-    });
+	hamburger.addEventListener('click', () => {
+		hamburger.classList.toggle('hamburger_active');
+		menu.classList.toggle('promo__nav-menu_active');
+	});
 
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('promo__nav-menu_active');
-        })
-    })
-})
+	menuItem.forEach(item => {
+		item.addEventListener('click', () => {
+			hamburger.classList.toggle('hamburger_active');
+			menu.classList.toggle('promo__nav-menu_active');
+		})
+	});
+
+	// slick slider 
+	// $('.testimonials__wrapper').slick({
+	// 	centerMode: true,
+	// 	centerPadding: '60px',
+	// 	slidesToShow: 3,
+	// 	responsive: [{
+	// 			breakpoint: 768,
+	// 			settings: {
+	// 				arrows: false,
+	// 				centerMode: true,
+	// 				centerPadding: '40px',
+	// 				slidesToShow: 3
+	// 			}
+	// 		},
+	// 		{
+	// 			breakpoint: 480,
+	// 			settings: {
+	// 				arrows: false,
+	// 				centerMode: true,
+	// 				centerPadding: '40px',
+	// 				slidesToShow: 1
+	// 			}
+	// 		}
+	// 	]
+	// });
+	$('.testimonials__wrapper').slick({
+		centerMode: true,
+		centerPadding: '200px',
+		infinite: true,
+		slidesToShow: 1,
+		prevArrow: '<button type="button" class="slick-prev"><img src="icons/prevArrow.png"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="icons/nextArrow.png"></button>',
+		responsive: [{
+				breakpoint: 769,
+				settings: {
+					arrows: false,
+					centerMode: true,
+					centerPadding: '140px',
+					slidesToShow: 1
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					arrows: false,
+					centerMode: true,
+					centerPadding: '40px',
+					slidesToShow: 1
+				}
+			}
+		]
+	});
+});
